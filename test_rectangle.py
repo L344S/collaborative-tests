406a624e147da3c5ad14c9bc756d6d013bedf15e
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-21 10:12:00 +0100

    added test_rectangle display() + moar tests

--- test_rectangle.py
+++ test_rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import unittest
+import sys
+from io import StringIO
 from models.rectangle import Rectangle
 
 """Change order of attribute according to your own"""
@@ -25,15 +27,53 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError) as err:
             rectangle = Rectangle("hello", 2, 3)
         self.assertEqual(str(err.exception), "width must be an integer")
+        with self.assertRaises(TypeError) as err1:
+            rectangle1 = Rectangle(1, "howdy", 3)
+        self.assertEqual(str(err1.exception), "height must be an integer")
+        with self.assertRaises(TypeError) as err2:
+            rectangle1 = Rectangle(1, 2, "hi")
+        self.assertEqual(str(err2.exception), "x must be an integer")
+        with self.assertRaises(TypeError) as err3:
+            rectangle1 = Rectangle(1, 2, 3, "sup'")
+        self.assertEqual(str(err3.exception), "y must be an integer")
 
     def test_rectangle_value_error(self):
         with self.assertRaises(ValueError) as err:
             rectangle = Rectangle(-2, 1)
         self.assertEqual(str(err.exception), "width must be > 0")
+        with self.assertRaises(ValueError) as err2:
+            rectangle = Rectangle(2, 0)
+        self.assertEqual(str(err2.exception), "height must be > 0")
+        with self.assertRaises(ValueError) as err3:
+            rectangle = Rectangle(0, 1)
+        self.assertEqual(str(err3.exception), "width must be > 0")
 
     def test_rectangle_area(self):
         rectangle = Rectangle(3, 2)
         self.assertEqual(rectangle.area(), 6)
+
+    def setUp(self):
+        """function to redirect stdout to capture print"""
+        self.held, sys.stdout = sys.stdout, StringIO()
+
+    def tearDown(self):
+        """function to restore original stdout after test"""
+        sys.stdout = self.held
+
+    def test_rectangle_display_without_x_and_y(self):
+        rectangle = Rectangle(3, 2, 0, 0)
+        rectangle.display()
+        self.assertEqual(sys.stdout.getvalue(), "###\n###\n")
+
+    def test_display_without_y(self):
+        rect = Rectangle(3, 2, 2, 0)
+        rect.display()
+        self.assertEqual(sys.stdout.getvalue(), "  ###\n  ###\n")
+
+    def test_display(self):
+        rect = Rectangle(2, 4, 1, 2)
+        rect.display()
+        self.assertEqual(sys.stdout.getvalue(), "\n\n ##\n ##\n ##\n ##\n")
 
     def test_rectangle_str(self):
         rectangle = Rectangle(3, 2, 1, 0, 10)
